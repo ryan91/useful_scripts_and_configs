ffc0e020c68896a8c0f07cdc0a0e7ac31dde1a20
Yannick Boekle <boekle@informatik.uni-tuebingen.de>
2019-02-04 13:43:16 +0100

    Removed old vim configs and added zshell script

--- zshell/install.sh
+++ zshell/install.sh
@@ -2,9 +2,15 @@
 
 set -e
 
-# install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/in
-stall.sh)"
+zrc="$HOME/.zshrc"
+theme="junkfood"
+sedcmd="s/ZSH_THEME=\".*\"/ZSH_THEME=\"$theme\"/1"
 
-# overwrite with custom config
-cp -f ./.zshrc $HOME
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# append user config
+cat ./.zshrc >> $HOME/.zshrc
+
+# set theme
+sed -i "$sedcmd" ~/.zshrc
